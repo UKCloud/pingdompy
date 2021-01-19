320273c2261956f5777f6a63835e82697bada98c
Jacob Gibbs <jgibbs@ukcloud.com>
2021-01-19 16:07:36 +0000

    updated README format, reworked significant parts of client.py

---
+++ README.md
@@ -0,0 +1,171 @@
+pingdompy
+=========
+
+
+Python library for interacting with Pingdom services (REST API and maintenance windows).
+
+
+Features
+--------
+
+
+* Check management: create, delete, update, list
+* Maintenance windows: create, delete, list
+* Fetching outage summaries
+
+Requirements
+------------
+
+
+* Pingdom account
+* requests (0.10.8 or newer)
+
+
+Installation
+------------
+
+```
+    pip install pingdompy
+```
+
+Usage
+-----
+
+The *client* object will allow you to interact with the API.
+
+```
+    >>> import pingdompy
+    >>> client = pingdompy.Client(apikey="your api key")
+```
+
+Checks
+------
+
+**List all checks:**
+
+The Pingdom Api can be called to recieve a comprehensive list of all checks visible to the api key specified:
+
+```
+    >>> checks = client.get_checks()
+        print(checks)
+
+    [{
+        "type": "http",
+        "id": 100,
+        "name": "mycheck",
+        "lasterrortime": 1297446423,
+        "lasttesttime": 1300977363,
+        "lastresponsetime": 355,
+        "status": "up",
+        "resolution": 0,
+        "hostname": "string",
+        "created": 0,
+        "tags": [{"name": "apache", "type": "a", "count": 2}],
+        "probe_filters": ["region: EU"],
+        "ipv6": true
+    }]
+```
+
+List checks with *production* and *frontend* tags:
+
+```
+    >>> client.get_checks(tags=[production,frontend])
+```
+
+**List specific check:**
+
+Pingdom can also be called to pull a more detailed list of a specific check when an ID is passed in:
+
+```
+    >>> check = client.get_check(checkid)
+    print(check)
+
+    {
+        "type": {},
+        "sendnotificationwhendown": 6,
+        "notifyagainevery": 5,
+        "notifywhenbackup": true,
+        "responsetime_threshold": 30000,
+        "custom_message": "Important check is down!",
+        "integrationids": [],
+        "id": 100,
+        "name": "mycheck",
+        "lasterrortime": 1297446423,
+        "lasttesttime": 1300977363,
+        "lastresponsetime": 355,
+        "status": "up",
+        "resolution": 0,
+        "hostname": "string",
+        "created": 0,
+        "tags": [],
+        "probe_filters": [],
+        "ipv6": true,
+        "verify_certificate": true,
+        "ssl_down_days_before": 0
+    }
+```
+
+**Create a check:**
+
+```
+    >>> changes = {
+            "name": "Check Check"
+            "host": "www.google.com",
+            "tags": [{"name": "pingdompy-test"},
+            "resolution": "60"
+            "type": "http"
+        }
+    >>> client.create_check(check_definition)
+```
+
+Refer to [this page](https://docs.pingdom.com/api/#tag/Checks/paths/~1checks/post) for the list of options that could also be implemented.
+
+**Integrations: (not yet implemented)**
+
+To enable/disable an integration plugins (like webhooks) use the field *integrationids* (array with integer ids to set or "null" tring to remove it)
+
+**Alert policies: (not yet implemented)**
+
+To bind an alerting policy use the field *alert_policy* (numeric id to set it or string "null" to disable alerts)
+
+
+**Update a check:**
+
+```
+    >>> client.update_check(check, {"paused": True})
+```
+
+this will return the updated checks' details for verification and a message confirming the update occured
+
+
+**Delete a check: (not yet implemented)**
+
+```
+    >>> client.delete_check(check)
+```
+
+
+Maintenance windows
+-------------------
+
+**Create a 1 hour maintenance window for production websites:**
+
+```
+    >>> start = datetime.datetime.now() + datetime.timedelta(minutes=10)
+    >>> end = start + datetime.timedelta(hours=1)
+
+    >>> window = client.create_maintenance({"checks": checks, "name": maint_name, \
+         "start": start, "stop": end, "uptime_ids": arg_uptimeid})
+```
+
+Setting the *checks* to *none* will allow you pass in an uptime ID rather than a tag
+
+
+**Delete future maintenance windows: (to be implemented)**
+
+```
+    >>> windows = client.get_maintenances(filters={"checks": checks, \ 
+        "after": datetime.datetime.now()}):
+    >>> for m in maintenances:
+        client.delete_maintenance(m)
+```
--- pingdompy/client.py
+++ pingdompy/client.py
@@ -17,49 +17,49 @@ class Client(object):
         :param apikey: Pingdom api key
         """
         self.api = Api(apikey, api_version)
-        # cache checks
-        self.checks = {}
-        self.maintenances = {}
-        for item in self.api.send('get', "checks", params={"include_tags": True})['checks']:
-            self.checks[item["name"]] = Check(self.api, json=item)
 
-    def get_check(self, name=None, _id=None):
-        if name:
-            return self.checks.get(name, None)
-        elif _id:
-            for _name, check in self.checks.items():
-                if check._id == _id:
-                    return check
-        else:
-            raise Exception("Missing name or _id")
+    def get_check(self, checkid, include_teams = False):
+        check = self.api.send(method="get", resource="checks", \
+        resourceid=checkid, params={"include_teams": include_teams})["check"]
+        return check
 
-    def get_checks(self, filters=None):
-        if filters is None:
-            return [c for c in self.checks.values()]
-
-        return [c for c in self.checks.values() if len(set(u + filters.get("status", c.status)
-                for u in filters.get("tags", [])).intersection(set([x['name'] + c.status for x in c.tags])))]
+    def get_checks(self, tags):
+        checks = self.api.send(method="get", resource = "checks", \
+        data=tags)["checks"]
+        return checks
 
     def create_check(self, obj):
-        c = Check(self.api, obj=obj)
-        data = c.to_json()
+        check = Check(self.api, obj=obj)
+        data = check.to_json()
         response = self.api.send(method='post', resource='checks', data=data)
-        c._id = int(response["check"]["id"])
-        c.from_json(self.api.send('get', "checks", response["check"]["id"])['check'])
-        self.checks[c.name] = c
-        return c
+        check._id = int(response["check"]["id"])
+        return check
 
     # def delete_check(self, check):
     #     if not check._id:
     #         raise Exception("CheckNotFound %s" % check.name)
     #     self.api.send(method='delete', resource='checks', resource_id=check._id)
-    #     self.checks.pop(check.name, None)
 
     def update_check(self, check, changes):
-        update = self.api.send(method='put', resource='checks', resource_id=check, data=changes)
-        response = [self.api.send(method='get', resource="checks", resource_id=check)['check'], update['message']]
-        return response
-     
+        ## Caches current version of check
+        if changes:
+            cached = self.api.send(method='get', resource="checks", resource_id=check)['check']
+            update = self.api.send(method='put', resource='checks', resource_id=check, data=changes)
+            ## Gets updated version of the check
+            verify = self.api.send(method='get', resource="checks", resource_id=check)['check']
+            if cached != verify and update['message'] == "Modification of check was successful!":
+                response = [update['message'], verify]
+                return response
+            elif cached == verify:
+                response = "There were no changes made!"
+                return response
+            else:
+                response = "There seems to be an issue with Pingdom!"
+                return  response
+        else:
+            response = "No changes were specified!"
+            return response
+ 
     def get_maintenance(self, window_id):
         value = str(window_id)
         response = self.api.send(method = 'get', resource = 'maintenance', resource_id = value)
@@ -71,33 +71,3 @@ class Client(object):
          response = self.api.send(method = 'post', resource = 'maintenance', data = value)
          window._id = response["maintenance"]["id"]
          return window
-
-    # def servertime(self):
-    #     return self.api.send(method='get', resource='servertime')['servertime']
-
-    # def get_summary_average(self, checkid, start=None, end=None, probes=None, include_uptime=None, by_country=None,
-    #                         by_probe=None):
-    #     params = {}
-    #     if start is not None:
-    #         params['from'] = start
-    #     if end is not None:
-    #         params['to'] = end
-    #     if probes is not None:
-    #         params['probes'] = probes
-    #     if include_uptime is not None:
-    #         params['includeuptime'] = include_uptime
-    #     if by_country is not None:
-    #         params['bycountry'] = by_country
-    #     if by_probe is not None:
-    #         params['byprobe'] = by_probe
-    #     return self.api.send('get', resource="summary.average", resource_id=checkid, params=params)
-
-    # def get_summary_outage(self, checkid, start=None, end=None, order="asc"):
-    #     params = {}
-    #     if start is not None:
-    #         params['from'] = start
-    #     if end is not None:
-    #         params['to'] = end
-    #     if order is not None:
-    #         params['order'] = order
-    #     return self.api.send('get', resource="summary.outage", resource_id=checkid, params=params)
